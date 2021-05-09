a7775cc1c0bcde89f3de0855c5e1ebfc7c7c9ff0
algo21-219040029 <219040029@link.cuhk.edu.cn>
2021-05-09 16:27:09 +0800

    20210509

---
+++ factor/basics.py
@@ -0,0 +1,221 @@
+basic_strings = ["Open","High","Low","Close",
+                "Volume","Avg","Status",
+                "Pb","Reason","Value","Amount","Rate",
+                "I000010","I000016","I000019","I000021",
+                 "I000160","I000300","I000905","I000906",
+                 "I399903","I399904","I930649","I930660",
+                 "I930661","IH00160","Weight","O1E1","O1E2","O1E3","O1O2"
+                ]
+
+basic_factors = ["Open","High","Low","Close", "PreClose",
+                 "AdjOpen","AdjHigh","AdjLow","AdjClose","AdjPreClose",
+                 "Volume","Amount","Avg","HaltStatus","StStatus",
+                 "AdjPb","AdjReason","MarketValue","CirculationMarketValue","DealAmount",
+                 "IcBenchmarkClose","IcBenchmarkOpen","IfBenchmarkOpen", "IfBenchmarkClose",
+                 "IhBenchmarkClose","IhBenchmarkOpen",
+                 "TurnoverRate","TurnoverValue","TurnoverVolume",
+                 "I000010","I000016","I000019","I000021",
+                 "I000160","I000300","I000905","I000906",
+                 "I399903","I399904","I930649","I930660",
+                 "I930661","IH00160","Ih50Weight","Csi300Weight","Ic500Weight",
+                 "O1E1","O1E2","O1E3","O1O2"]
+
+
+def I000010():
+    return "I000010()"
+
+
+def I000016():
+    return "I000016()"
+
+
+def I000019():
+    return "I000019()"
+
+
+def I000021():
+    return "I000021()"
+
+
+def I000160():
+    return "I000160()"
+
+
+def I000300():
+    return "I000300()"
+
+
+def I000905():
+    return "I000905()"
+
+
+def I000906():
+    return "I000906()"
+
+
+def I399903():
+    return "I399903()"
+
+
+def I399904():
+    return "I399904()"
+
+
+def I930649():
+    return "I930649()"
+
+
+def I930660():
+    return "I930660()"
+
+
+def I930661():
+    return "I930661()"
+
+
+def IH00160():
+    return "IH00160()"
+
+
+def Open():
+    return "Open()"
+
+
+def High():
+    return "High()"
+
+
+def Low():
+    return "Low()"
+
+
+def Close():
+    return "Close()"
+
+
+def Volume():
+    return "Volume()"
+
+
+def Amount():
+    return "Amount()"
+
+
+def AdjOpen():
+    return "AdjOpen()"
+
+
+def AdjHigh():
+    return "AdjHigh()"
+
+
+def AdjLow():
+    return "AdjLow()"
+
+
+def AdjClose():
+    return "AdjClose()"
+
+
+def AdjPreClose():
+    return "AdjPreClose()"
+
+
+def AdjVolume():
+    return "AdjVolume()"
+
+
+def Avg():
+    return "Avg()"
+
+
+def HaltStatus():
+    return "HaltStatus()"
+
+
+def StStatus():
+    return "StStatus()"
+
+
+def AdjPb():
+    return "AdjPb()"
+
+
+def AdjReason():
+    return "AdjReason()"
+
+
+def MarketValue():
+    return "MarketValue()"
+
+
+def CirculationMarketValue():
+    return "CirculationMarketValue()"
+
+
+def DealAmount():
+    return "DealAmount()"
+
+
+def IcBenchmarkOpen():
+    return "IcBenchmarkOpen()"
+
+
+def IcBenchmarkClose():
+    return "IcBenchmarkClose()"
+
+
+def IfBenchmarkOpen():
+    return "IfBenchmarkOpen()"
+
+
+def IfBenchmarkClose():
+    return "IfBenchmarkClose()"
+
+
+def IhBenchmarkOpen():
+    return "IhBenchmarkOpen()"
+
+
+def IhBenchmarkClose():
+    return "IhBenchmarkClose()"
+
+
+def TurnoverRate():
+    return "TurnoverRate()"
+
+
+def TurnoverValue():
+    return "TurnoverValue()"
+
+
+def TurnoverVolume():
+    return "TurnoverVolume()"
+
+
+def Ih50Weight():
+    return "Ih50Weight()"
+
+
+def Csi300Weight():
+    return "Csi300Weight()"
+
+
+def Ic500Weight():
+    return "Ic500Weight()"
+
+
+def O1E1():
+    return "O1E1()"
+
+
+def O1E2():
+    return "O1E2()"
+
+
+def O1E3():
+    return "O1E3()"
+
+
+def OQO2():
+    return "O1O2()"
---
+++ factor/compute_engine.py
@@ -0,0 +1,16 @@
+from factor.basics import basic_factors
+
+class ComputeEngine:
+
+    def __init__(self) -> None:
+        """Constructor"""
+        pass
+
+    def init_compute_engine(self) -> None:
+        """
+        初始化因子计算
+
+        Returns
+        -------
+
+        """
